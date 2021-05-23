c2ca235359d3106f322c1f5d71af18e50594c4c0
Davi Fernando Rosa <davir0514@gmail.com>
2021-05-22 21:54:51 -0300

    Arquivo

---
+++ LoopFor.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package teste3;
+
+/**
+ *A instrução ++i, incrementa a variável iantes dela ser utilizada no comando. 
+ * Já a instrução i++, incrementa a variável idepois dela ser utilizada no comando.
+ * E se você incrementar uma variável e atribuir o resultado a ela mesma, em um comando,
+ * o resultado da instrução é o valor da variável após o incremento.
+ * @author Davi
+ */
+public class LoopFor {
+    
+    public static void main(String[]args){
+    
+        for(int i =0 ; i<=5; i++)
+            System.out.println(i);
+    }
+
+    
+}
